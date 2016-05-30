c4f81f2d742dda981e419ae8239f8658de4b3a18
Sowiarz <pavel.horubala@gmail.com>
2016-05-30 13:06:53 +0200

    Zadanie 2
    Niedokończone zadanie 2 - zrobię to zadanie i podeśle w najbliższym
    czasie

---
+++ Zadanie_2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Zadanie_2
+{
+	class MainClass
+	{
+		[STAThread]
+		public static void Main (string[] args)
+		{
+			var dialog = new OpenFileDialog
+			{
+				Multiselect = false,
+				Title = "Open Hex Document",
+			};
+			using (dialog)
+			{
+				if (dialog.ShowDialog() == DialogResult.OK)
+				{
+					FileStream fs = new FileStream(dialog.FileName, FileMode.Open);
+
+					int hexIn;
+					String hexText = string.Empty;
+
+					for (int i = 0; (hexIn = fs.ReadByte()) != -1; i++){
+						hexText = string.Format("{0:X2}", hexIn);
+					}
+					Console.WriteLine (hexText);
+				}
+			}
+
+		}
+
+	}
+}
